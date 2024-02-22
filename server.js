32afdddfd59149879fbe1693608bf47be0e17f29
NrNop <nuranabipour@gmail.com>
2024-02-22 19:35:18 +0330

    update

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
-const userRoute=require("./routes/user");
-app.use('/user',userRoute);
-
-const movieRoute=require("./routes/movie");
-app.use('/movie',movieRoute);
-
+app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173' // Adjust this to match your React app's origin
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+const userRoute=require("./routes/user");
+app.use('/users',userRoute);
+
+const movieRoute=require("./routes/movie");
+app.use('/movies',movieRoute);
+
+
 
 
 app.listen(port, () => console.log(`Server is ready at http://localhost:${port}`));
